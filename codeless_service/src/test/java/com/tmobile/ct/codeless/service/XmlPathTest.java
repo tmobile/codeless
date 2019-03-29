68714d6a2049acdbbe872c4d1e16b86d2256cadc
juliozev <Julio.Zevallos1@T-Mobile.com>
2019-03-29 13:46:38 -0700

    added unit tests for getXmlPath, getActual

---
+++ codeless_service/src/test/java/com/tmobile/ct/codeless/service/XmlPathTest.java
@@ -0,0 +1,31 @@
+package com.tmobile.ct.codeless.service;
+
+import com.tmobile.ct.codeless.service.Call;
+import com.tmobile.ct.codeless.service.HttpRequest;
+import com.tmobile.ct.codeless.service.accessor.response.XmlPathAccessor;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.tmobile.ct.codeless.service.httpclient.HttpMethod;
+import com.tmobile.ct.codeless.service.restassured.RestAssuredHttpClient;
+import org.junit.Before;
+import org.junit.Test;
+
+public class XmlPathTest extends BaseWiremockTest{
+
+    XmlPathAccessor accessor;
+    Call call;
+    @Before
+    public void shouldInstantiate() {
+        HttpRequest req = buildXmlRequest();
+        req.setHttpMethod(HttpMethod.GET);
+        call = new Call(new RestAssuredHttpClient(), req, 0);
+        call.run();
+        accessor = new XmlPathAccessor("message", String.class);
+        assertThat(accessor).isNotNull();
+    }
+
+    @Test
+    public void shouldGetActual(){
+        assertThat(accessor.getActual(call)).isEqualTo("testing");
+    }
+}
